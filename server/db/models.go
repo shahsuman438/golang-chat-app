783895e0b51d94bb7b519983f94a679a9a0f923f
suman sah <shahsuman438@gmail.com>
2022-09-08 15:57:37 +0545

    added unique constrain to email and manage error responsed

--- server/db/models.go
+++ server/db/models.go
@@ -1,11 +1,10 @@
 package db
 
-
 type User struct {
-	ID       int    `sql:"id,pk" json:"id"`
-	Email    string `sql:"email,unique" json:"email"`
-	UserName string `sql:"user_name" json:"userName"`
-	Password string `sql:"password" json:"password"`
+	ID       int    `pg:"id,pk" sql:"id,pk" json:"id"`
+	Email    string `pg:"email,unique" sql:"email, unique" json:"email"`
+	UserName string `pg:"user_name" sql:"user_name" json:"userName"`
+	Password string `pg:"password" sql:"password" json:"password"`
 }
 
 type Chat struct {
